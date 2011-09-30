ae0e39e8b963adad9db49232b8e8b010e167e05f
prabirshrestha <mail@prabir.me>
2011-09-30 23:09:37 +0545

    + hg/hg-git.symlink

--- install.cmd
+++ install.cmd
@@ -1,6 +1,6 @@
 @echo off
 PUSHD "%~dp0"
-D:\Prabir\HOME\tools\ruby-1.9.2-p0-i386-mingw32\bin\rake.bat install[mklink]
+D:\apps\ruby-1.9.2-p0-i386-mingw32\bin\rake.bat install[mklink]
 POPD
 pause
 
