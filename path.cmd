abfda4784e84403c31f478bdea466131ea126c9d
prabirshrestha <mail@prabir.me>
2011-11-24 23:43:48 +0545

    + path.cmd

---
+++ path.cmd
@@ -0,0 +1,3 @@
+@echo off
+@echo %USERPROFILE%\apps\node;%USERPROFILE%\apps\git;%USERPROFILE%\apps\git\cmd;%USERPROFILE%\apps\kdiff3;%USERPROFILE%\apps\pik;%USERPROFILE%\apps\vim\vim73;%USERPROFILE%\apps\SublimeText;%USERPROFILE%\apps\tortoisehg;%USERPROFILE%\apps\filezilla;
+@pause
