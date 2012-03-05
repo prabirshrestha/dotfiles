e7ff663b4b2caf1010a92eccb3d2d2cd9cd76921
prabirshrestha <mail@prabir.me>
2012-03-05 10:54:22 -0500

    fixed bash/prompt.bash for mingw

--- bash/prompt.bash
+++ bash/prompt.bash
@@ -1,1 +1,11 @@
-export PS1="\n\[\033[38m\]\u@\h\[\033[01;34m\] \w \[\033[31m\]\`ruby -e \"print (%x{git branch 2> /dev/null}.grep(/^\*/).first || '').gsub(/^\* (.+)$/, '(\1) ')\"\`\[\033[37m\]\n$\[\033[00m\] "
+if [ -z "$MSYSTEM" ]; then
+  MSYSTEM=MINGW32
+fi
+
+if [ $MSYSTEM == MINGW32 ]; then
+  export PS1='\[\033]0;$MSYSTEM:\w\007
+\033[32m\]\u@\h \[\033[33m\w$(__git_ps1)\033[0m\]
+$ '
+else
+  export PS1="\n\[\033[38m\]\u@\h\[\033[01;34m\] \w \[\033[31m\]\`ruby -e \"print (%x{git branch 2> /dev/null}.grep(/^\*/).first || '').gsub(/^\* (.+)$/, '(\1) ')\"\`\[\033[37m\]\n$\[\033[00m\] "
+fi
