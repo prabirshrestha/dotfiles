71377a3b75ab9321d829aaa882843081d431d59a
prabirshrestha <mail@prabir.me>
2012-03-05 09:53:51 -0500

    updated ruby path to ruby-1.9.3-p125-i386-mingw32

--- install.cmd
+++ install.cmd
@@ -1,5 +1,5 @@
 @echo off
 PUSHD "%~dp0"
-"%USERPROFILE%\apps\ruby-1.9.2-p0-i386-mingw32\bin\rake.bat" install[mklink]
+"%USERPROFILE%\apps\ruby-1.9.3-p125-i386-mingw32\bin\rake.bat" install[mklink]
 POPD
 pause
