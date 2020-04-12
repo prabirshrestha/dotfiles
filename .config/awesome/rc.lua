892de5af435147a7f6144660e370caf6ac275f85
Prabir Shrestha <mail@prabir.me>
2020-04-12 05:34:17 +0000

    use alt as mod1

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -57,7 +57,7 @@ editor_cmd = terminal .. " -e " .. editor
 -- If you do not like this or do not have such a key,
 -- I suggest you to remap Mod4 to another key using xmodmap or other tools.
 -- However, you can use another modifier like Mod1, but it may interact with others.
-modkey = "Mod4"
+modkey = "Mod1"
 
 -- Table of layouts to cover with awful.layout.inc, order matters.
 awful.layout.layouts = {
@@ -91,6 +91,7 @@ myawesomemenu = {
 }
 
 mymainmenu = awful.menu({ items = { { "awesome", myawesomemenu, beautiful.awesome_icon },
+                                    { "Gvim", "gvim" },
                                     { "open terminal", terminal }
                                   }
                         })
