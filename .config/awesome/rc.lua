0cee20cd00a0b0d32c4e53ac2ffe65bf67be5c97
Prabir Shrestha <mail@prabir.me>
2020-04-13 00:53:07 +0000

    hide awesomewm titlebars

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -513,7 +513,7 @@ awful.rules.rules = {
 
     -- Add titlebars to normal clients and dialogs
     { rule_any = {type = { "normal", "dialog" }
-      }, properties = { titlebars_enabled = true }
+      }, properties = { titlebars_enabled = false }
     },
 
     -- Set Firefox to always map on the tag named "2" on screen 1.
