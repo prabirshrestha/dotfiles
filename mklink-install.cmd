9d5ddfad2a1928ed6eede42379d0c02c8895a217
Prabir Shrestha <mail@prabir.me>
2020-06-21 20:38:38 +0000

    remove mklink-install.cmd

--- mklink-install.cmd
+++
@@ -1,7 +0,0 @@
-@echo off
-PUSHD "%~dp0"
-mklink /d "%USERPROFILE%/apps" "C:/apps/"
-rem mklink /d "%USERPROFILE%/.VirtualBox" "d:/Prabir/.VirtualBox"
-rem mklink /d "%USERPROFILE%/VirtualBox VMs" "d:/Prabir/VirtualBox VMs"
-POPD
-pause
