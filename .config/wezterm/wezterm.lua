0b310a925ddf4d440d494a619d707d9afb69af0c
Prabir Shrestha <mail@prabir.me>
2020-09-25 23:21:38 -0700

    make wezterm behave like tmux

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -31,7 +31,7 @@ local config = {
         "Fira Code",
         "Consolas"
     }),
-    font_size = 14.0,
+    font_size = 12.0,
     launch_menu = launch_menu,
     colors = {
         foreground = "#839496",
@@ -44,9 +44,30 @@ local config = {
         brights = {"#002b36","#cb4b16","#586e75","#657b83","#839496","#6c71c4","#93a1a1","#fdf6e3"},
     },
     set_environment_variables = env,
+    leader = { key="a", mods="CTRL" },
     keys = {
-        { key = "_", mods = "ALT|SHIFT", action=wezterm.action{SplitVertical={SpawnCommand={Domain=0}}} },
-        { key = "|", mods = "ALT|SHIFT", action=wezterm.action{SplitHorizontal={SpawnCommand={Domain=0}}} },
+        { key = "-", mods = "LEADER", action=wezterm.action{SplitVertical={domain="CurrentPaneDomain"}} },
+        { key = "\\", mods = "LEADER", action=wezterm.action{SplitHorizontal={domain="CurrentPaneDomain"}} },
+        { key = "z", mods = "LEADER", action="TogglePaneZoomState" },
+        { key = "c", mods = "LEADER", action=wezterm.action{SpawnTab="CurrentPaneDomain"}},
+        { key = "h", mods = "LEADER", action=wezterm.action{ActivatePaneDirection="Left"}},
+        { key = "j", mods = "LEADER", action=wezterm.action{ActivatePaneDirection="Down"}},
+        { key = "k", mods = "LEADER", action=wezterm.action{ActivatePaneDirection="Up"}},
+        { key = "l", mods = "LEADER", action=wezterm.action{ActivatePaneDirection="Right"}},
+        { key = "H", mods = "LEADER|SHIFT", action=wezterm.action{AdjustPaneSize={"Left", 5}}},
+        { key = "J", mods = "LEADER|SHIFT", action=wezterm.action{AdjustPaneSize={"Down", 5}}},
+        { key = "K", mods = "LEADER|SHIFT", action=wezterm.action{AdjustPaneSize={"Up", 5}}},
+        { key = "L", mods = "LEADER|SHIFT", action=wezterm.action{AdjustPaneSize={"Right", 5}}},
+        { key = "1", mods = "LEADER", action=wezterm.action{ActivateTab=0}},
+        { key = "2", mods = "LEADER", action=wezterm.action{ActivateTab=1}},
+        { key = "3", mods = "LEADER", action=wezterm.action{ActivateTab=2}},
+        { key = "4", mods = "LEADER", action=wezterm.action{ActivateTab=3}},
+        { key = "5", mods = "LEADER", action=wezterm.action{ActivateTab=4}},
+        { key = "6", mods = "LEADER", action=wezterm.action{ActivateTab=5}},
+        { key = "7", mods = "LEADER", action=wezterm.action{ActivateTab=6}},
+        { key = "8", mods = "LEADER", action=wezterm.action{ActivateTab=7}},
+        { key = "9", mods = "LEADER", action=wezterm.action{ActivateTab=8}},
+        { key = "&", mods = "LEADER|SHIFT", action="CloseCurrentTab"},
     }
 }
 
