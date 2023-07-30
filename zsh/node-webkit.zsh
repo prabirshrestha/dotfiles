a03b961294b94640c053c8b2b8a77bb48a7e76e9
Prabir Shrestha <mail@prabir.me>
2023-07-29 21:42:58 -0700

    remove old zsh folder

--- zsh/node-webkit.zsh
+++
@@ -1,5 +0,0 @@
-NODE_WEBKIT="/Applications/node-webkit.app/Contents/MacOS/node-webkit"
-
-if [ -f $NODE_WEBKIT ]; then
-    alias nw=$NODE_WEBKIT
-fi
--- zsh/rvm.zsh
+++
@@ -1,5 +0,0 @@
-# rvm
-if [ -f "$HOME/.rvm/scripts/rvm" ]; then
-	source "$HOME/.rvm/scripts/rvm"
-	rvm use default > /dev/null
-fi
