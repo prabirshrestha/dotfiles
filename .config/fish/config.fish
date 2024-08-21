35884012ce4f775e7ba9abf01ec7cbf53afcab98
Prabir Shrestha <mail@prabir.me>
2024-08-20 20:31:58 -0700

    Update fish.config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,4 +1,19 @@
 # disable fish greeting
 set fish_greeting
 
+fish_add_path ~/.dotfiles/bin
 fish_add_path /opt/homebrew/bin
+fish_add_path /opt/homebrew/sbin
+
+fish_add_path ~/.bun/bin
+fish_add_path ~/.cargo/bin
+
+type -q zoxide; and zoxide init fish | source
+
+alias g "git"
+alias gs "git status"
+alias gup "git fetch --all && git rebase"
+
+fish_add_path ~/.proto/bin/shims
+fish_add_path ~/.proto/bin
+type -q proto; and proto activate fish | source
