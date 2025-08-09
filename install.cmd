456ac80f16c0667fe09120b4c4cc88ae2a8af9fc
Prabir Shrestha <mail@prabir.me>
2025-08-08 21:05:23 -0700

    add scoop buckets

--- install.cmd
+++ install.cmd
@@ -1,5 +1,18 @@
+@set scoop_buckets=^
+  extras
+
+
+@for %%i in (%scoop_buckets%) do @(
+    scoop bucket list | findstr /i "%%i" >nul || (
+        echo Adding bucket: %%i
+        scoop bucket add %%i
+    )
+)
+
 @set scoop_packages=^
+  7zip^
+  fzf^
   sudo^
   zoxide
 
-@for %%i in (%scoop_packages%) do @scoop install %%i 2>nul
+@for %%i in (%scoop_packages%) do scoop install %%i 2>nul
